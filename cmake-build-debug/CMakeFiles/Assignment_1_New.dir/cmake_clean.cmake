9efa5fae6bc4bb8ccd1d8be8ef358cc30a607f20
Amit <steinam@post.bgu.ac.il>
2020-11-09 17:25:37 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/Assignment_1_New.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assignment_1_New"
+  "Assignment_1_New.pdb"
+  "CMakeFiles/Assignment_1_New.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_1_New.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Assignment_1_New.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Assignment_1_New.
+# This may be replaced when dependencies are built.
